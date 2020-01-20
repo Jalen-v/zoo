69c9f48ff63cd2ff8382018b24b5679b470a7337
sunjinliang <sunjinliang@xiaomi.com>
2020-01-20 15:53:26 +0800

    add description

--- leetcode/src/main/java/com/fight/leetcode/A0119_2_SingleNumber.java
+++ leetcode/src/main/java/com/fight/leetcode/A0119_2_SingleNumber.java
@@ -1,17 +1,34 @@
 package com.fight.leetcode;
 
+/**
+ * 136. 只出现一次的数字
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 说明： 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ * 输入: [2,2,1]
+ * 输出: 1
+ *
+ * 示例 2:
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/single-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class A0119_2_SingleNumber {
     public static void main(String[] args) {
         Solution solution = new A0119_2_SingleNumber().new Solution();
-        int[] nums = {2,2,1};
+        int[] nums = {2, 2, 1};
         int i = solution.singleNumber(nums);
         System.out.println(i);
 
-        System.out.println(123^132);
-        System.out.println(123^123);
-        System.out.println(10^5);
-        System.out.println(10^14);
-        System.out.println(12^3);
+        System.out.println(123 ^ 132);
+        System.out.println(123 ^ 123);
+        System.out.println(10 ^ 5);
+        System.out.println(10 ^ 14);
+        System.out.println(12 ^ 3);
     }
 
     class Solution {
--- leetcode/src/main/java/com/fight/leetcode/A0120_1_MinStack.java
+++ leetcode/src/main/java/com/fight/leetcode/A0120_1_MinStack.java
@@ -2,6 +2,22 @@ package com.fight.leetcode;
 
 import java.util.Stack;
 
+/**
+ * 155. 最小栈
+ * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * push(x) -- 将元素 x 推入栈中。
+ * pop() -- 删除栈顶的元素。
+ * top() -- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-stack
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * https://leetcode-cn.com/problems/min-stack/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-38/
+ */
 public class A0120_1_MinStack {
 
     public static void main(String[] args) {
@@ -13,7 +29,9 @@ public class A0120_1_MinStack {
         private Stack<Integer> stack;
         private Stack<Integer> minStack;
 
-        /** initialize your data structure here. */
+        /**
+         * initialize your data structure here.
+         */
         public MinStack() {
             stack = new Stack<>();
             minStack = new Stack<>();
@@ -33,7 +51,7 @@ public class A0120_1_MinStack {
 
         public void pop() {
             Integer pop = stack.pop();
-            if(!stack.isEmpty()) {
+            if (!stack.isEmpty()) {
                 if (pop.equals(minStack.peek())) {
                     minStack.pop();
                 }
