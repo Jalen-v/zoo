2844dae72f58b33091cbddea526f58ad1f9b51a1
sunjinliang <sunjinliang@xiaomi.com>
2020-04-07 17:59:49 +0800

    modify

---
+++ leetcode/src/test/java/com/zoo/TmpTest.java
@@ -0,0 +1,14 @@
+package com.zoo;
+
+import org.junit.Test;
+
+public class TmpTest {
+
+    @Test
+    public void test1() {
+        Character ch = 'a';
+        String str = "adfa";
+        System.out.println(str.charAt(1));
+        String s1 = String.valueOf(str.charAt(1));
+    }
+}
