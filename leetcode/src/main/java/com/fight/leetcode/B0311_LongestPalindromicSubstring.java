6daa7fddf2189abd50e955bd7f5d373d2ba509f3
jalen <jalen_v@163.com>
2020-04-07 19:39:26 +0800

    commit

--- leetcode/src/main/java/com/fight/leetcode/B0311_LongestPalindromicSubstring.java
+++ leetcode/src/main/java/com/fight/leetcode/B0311_LongestPalindromicSubstring.java
@@ -2,7 +2,7 @@ package com.fight.leetcode;
 
 /**
  *  5. 最长回文子串
- *  
+ *
  *  给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
  *
  * 示例 1：
@@ -19,7 +19,7 @@ package com.fight.leetcode;
  * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class B_0311_LongestPalindromicSubstring {
+public class B0311_LongestPalindromicSubstring {
     public static void main(String[] args) {
     }
 
